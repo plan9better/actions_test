f42a92b006f5531f5ae5058dd62657ae16b38034
plan9better <plan9better@proton.me>
2025-02-26 11:22:20 +0100

    Update test_output.sh

--- test_output.sh
+++ test_output.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-output=$(diff <(result/bin/hello.out) <(echo "HelloOCaml!"))
+output=$(diff <(result/bin/hello.out) <(echo "Hello OCaml!"))
 
 if [[ -z "$output" ]]; then
   echo "Command has no output"
